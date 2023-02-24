22b6b969a539b2f89a5b8be26ea1cc964e16532b
BrageDrage909 <bfu01.bb@gmail.com>
2023-02-24 10:30:36 +0100

    upgrade

--- index.php
+++ index.php
@@ -5,9 +5,9 @@ include("connection.php");
 include("functions.php");
 $user_data = check_login($con);
 
-// Sjekker om poengsummen er blitt opptattert"
+// Sjekker om poengsummen er blitt oppdatert
 if (isset($_POST["score"])) {
-  // henter den opptattert scoren
+  // henter den oppdaterte scoren
   $score = intval($_POST["score"]);
 
   // lagrer score i databasen med $user_data sånn at det blir lagret i din bruker
@@ -16,8 +16,18 @@ if (isset($_POST["score"])) {
   $stmt->bind_param("ii", $score, $user_data["id"]);
   $stmt->execute();
 }
+if (isset($_POST["amount1"])) {
+  // henter den oppdaterte amount1-verdien
+  $amount1 = intval($_POST["amount1"]);
 
+  // lagrer amount1 i databasen med $user_data sånn at det blir lagret i din bruker
+  $query = "UPDATE users SET amount1 = ? WHERE id = ?";
+  $stmt = $con->prepare($query);
+  $stmt->bind_param("ii", $amount1, $user_data["id"]);
+  $stmt->execute();
+}
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -27,16 +37,29 @@ if (isset($_POST["score"])) {
 </head>
 <body>
 <h1>Amalie Skram Clicker</h1>
- <div class ="div-grid"> 
+<div class ="div-grid"> 
   <div class="clicker"> 
-   <p>Score: <span id ="score"><?php echo $user_data['cookies']; ?></span></p>  
+   <h2>Score: <span id ="score"><?php echo $user_data['cookies']; ?></span></h2> 
    <img src="/javascript-clicker-game/images/clicker.png" height="270px" width="300px" onclick="addToScore();"> 
-   <script>var score = <?php echo $user_data['cookies']; ?>;</script>
+   <script>
+   var score = <?php echo $user_data['cookies']; ?>;
+   var amount1 = <?php echo $user_data['amount1']; ?>;
+   </script>
   </div>
-  <div class="upgrade">
-     <h1>Upgrade Clicks</h1> 
-     <button onclick="UpgradeOne()"> +1 (<span id ="score"><?php echo $user_data['upgrade1']; ?></span>) </button>
-    </div>
+   <div class="upgrade">
+     <h2>Upgrade Clicks</h2> 
+     <div class="div-grid-u">
+        <div>
+         <button onclick="Upgrade1();">  +1 (<span id ="amount1"><?php echo $user_data['amount1']; ?></span>)</button>
+        </div>
+        <div>
+         <button onclick="UpgradeOne();">  +1 (<span id ="amount1"><?php echo $user_data['amount1']; ?></span>)</button>
+        </div>
+        <div>
+         <button onclick="UpgradeOne();">  +1 (<span id ="amount1"><?php echo $user_data['amount1']; ?></span>)</button>
+        </div>
+     </div>
+   </div>
   </div>
  </div> 
  <div class="logout">
--- javascript/cookies-index.js
+++ javascript/cookies-index.js
@@ -1,5 +1,5 @@
 function addToScore() {
-  score += 1;
+  score += amount1 + 1;
   document.getElementById('score').innerHTML = score;
   //Sender den opptattert scoren til php scriptet
   var xhr = new XMLHttpRequest();
@@ -13,19 +13,22 @@ function addToScore() {
   xhr.send("score=" + score);
 }
 
-function UpgradeOne() {
- if (score >= 100) {
-  score -= 100;
-  document.getElementById('score').innerHTML = score;
-  //Sender den opptattert scoren til php scriptet
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", location.href, true);
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-      console.log("You got another upgrade");
-    }
-  } 
-  xhr.send("score=" + score);
+function Upgrade1() {
+  if (score >= 100) {
+   score -= 100 ;
+   amount1 += 1
+   document.getElementById('score').innerHTML = score;
+   document.getElementById('amount1').innerHTML = amount1;
+   //Sender den oppdaterte scoren og amount1 til php scriptet
+   var xhr = new XMLHttpRequest();
+   xhr.open("POST", location.href, true);
+   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+   xhr.onreadystatechange = function() {
+     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+       console.log("You got another upgrade", amount1);
+     }
+   } 
+   xhr.send("score=" + score + "&amount1=" + amount1);
+   return price
+  }
  }
-}
