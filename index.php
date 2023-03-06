49971a771ca55109db0ed029c7417301852e4935
BrageDrage909 <bfu01.bb@gmail.com>
2023-03-06 13:30:39 +0100

    update

--- index.php
+++ index.php
@@ -36,13 +36,33 @@ if (isset($_POST["amount10"])) {
   $stmt->bind_param("ii", $amount10, $user_data["id"]);
   $stmt->execute();
 }
+if (isset($_POST["amount100"])) {
+  // henter den oppdaterte amount100-verdien
+  $amount100 = intval($_POST["amount100"]);
+
+  // lagrer amount100 i databasen med $user_data sånn at det blir lagret i din bruker
+  $query = "UPDATE users SET amount100 = ? WHERE id = ?";
+  $stmt = $con->prepare($query);
+  $stmt->bind_param("ii", $amount100, $user_data["id"]);
+  $stmt->execute();
+}
+if (isset($_POST["amount1000"])) {
+  // henter den oppdaterte amount100-verdien
+  $amount1000 = intval($_POST["amount1000"]);
+
+  // lagrer amount100 i databasen med $user_data sånn at det blir lagret i din bruker
+  $query = "UPDATE users SET amount1000 = ? WHERE id = ?";
+  $stmt = $con->prepare($query);
+  $stmt->bind_param("ii", $amount1000, $user_data["id"]);
+  $stmt->execute();
+}
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
   <title> Score: <?php echo $user_data['cookies']; ?></title>
-  <link rel="stylesheet" href="/javascript-clicker-game/css/cookie-index.css">
+  <link rel="stylesheet" href="/javascript-clicker-game/css/cookie_index.css">
   <script src="/javascript-clicker-game/javascript/cookies-index.js"></script>
 </head>
 <body>
@@ -50,25 +70,44 @@ if (isset($_POST["amount10"])) {
 <div class ="div-grid"> 
   <div class="clicker"> 
    <h2>Score: <span id ="score"><?php echo $user_data['cookies']; ?></span></h2> 
+   <p class="SPC">SPC: <span id="spc-verdi"> </p>
    <img src="/javascript-clicker-game/images/clicker.png" height="270px" width="300px" onclick="addToScore();"> 
    <script>
+   // Det går ikke å bruke i php echo i cookies-index.js av enn eller annnen grunn derfor er det her og ikke i cookies-index.js filen
    var score = <?php echo $user_data['cookies']; ?>;
    var amount1 = <?php echo $user_data['amount1']; ?>;
    var amount10 = <?php echo $user_data['amount10']; ?>;
    var amount100 = <?php echo $user_data['amount100']; ?>;
-   </script>
+   var amount1000 = <?php echo $user_data['amount1000']; ?>;
+   var spc = 1 + amount1 + (amount10 * 10) + (amount100 * 100) + (amount1000 * 1000);
+   
+   document.getElementById("spc-verdi").textContent = spc;
+   
+   function updateSPCValue() {
+      var newSPC = 1 + amount1 + (amount10 * 10)  + (amount100 * 100) + (amount1000 * 1000);
+      if (newSPC !== spc) {
+         spc = newSPC;
+         document.getElementById("spc-verdi").textContent = spc;
+      }
+   }
+   
+   setInterval(updateSPCValue, 10); //Opptattering vært 10 ms
+</script>
+
   </div>
    <div class="upgrade">
      <h2>Upgrade Clicks</h2> 
-     <div class="div-grid-u">
-        <div class="+1">
+        <div class="U1">
          <button onclick="Upgrade1();">  +1 (<span id ="amount1"><?php echo $user_data['amount1']; ?></span>)</button>
         </div>
-        <div class="+10">
+        <div class="U10">
          <button onclick="Upgrade10();">  +10 (<span id ="amount10"><?php echo $user_data['amount10']; ?></span>)</button>
         </div>
-        <div class="+100">
-         <button onclick="();">  100+ (<span id ="amount100"><?php echo $user_data['amount100']; ?></span>)</button>
+        <div class="U100">
+         <button onclick="Upgrade100();">  +100 (<span id ="amount100"><?php echo $user_data['amount100']; ?></span>)</button>        
+        </div>
+        <div class="U1000">
+         <button onclick="Upgrade1000();">  +1000 (<span id ="amount1000"><?php echo $user_data['amount1000']; ?></span>)</button>
         </div>
      </div>
    </div>
--- javascript/cookies-index.js
+++ javascript/cookies-index.js
@@ -1,5 +1,10 @@
+let price1 = 100
+let price10 = 1000
+let price100 = 10000
+let price1000 = 100000
+
 function addToScore() {
-  score += amount1 + (amount10 * 10) + 1;
+  score += amount1 + (amount10 * 10) + (amount100* 100) + (amount1000 * 1000)+ 1;
   document.getElementById('score').innerHTML = score;
   //Sender den opptattert scoren til php scriptet
   var xhr = new XMLHttpRequest();
@@ -15,10 +20,11 @@ function addToScore() {
 
 function Upgrade1() {
   if (score >= 100) {
-   score -= 100;
+   score -= price1;
    amount1 += 1
    document.getElementById('score').innerHTML = score;
    document.getElementById('amount1').innerHTML = amount1;
+   }  
    //Sender den oppdaterte scoren og amount1 til php scriptet
    var xhr = new XMLHttpRequest();
    xhr.open("POST", location.href, true);
@@ -30,11 +36,11 @@ function Upgrade1() {
    } 
    xhr.send("score=" + score + "&amount1=" + amount1);
   }
- }
+
  
 function Upgrade10() {
   if (score >= 1000) {
-   score -= 1000;
+   score -= price10;
    amount10 += 1
    document.getElementById('score').innerHTML = score;
    document.getElementById('amount10').innerHTML = amount10;
@@ -48,6 +54,44 @@ function Upgrade10() {
      }
    } 
    xhr.send("score=" + score + "&amount10=" + amount10);
+  }
+ }
+
+ function Upgrade100() {
+  if (score >= 10000) {
+   score -= price100;
+   amount100 += 1
+   document.getElementById('score').innerHTML = score;
+   document.getElementById('amount100').innerHTML = amount100;
+   //Sender den oppdaterte scoren og amount100 til php scriptet
+   var xhr = new XMLHttpRequest();
+   xhr.open("POST", location.href, true);
+   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+   xhr.onreadystatechange = function() {
+     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+       console.log("You got another upgrade", amount100);
+     }
+   } 
+   xhr.send("score=" + score + "&amount100=" + amount100);
+  }
+ }
+
+ function Upgrade1000() {
+  if (score >= 100000) {
+   score -= price1000;
+   amount1000 += 1
+   document.getElementById('score').innerHTML = score;
+   document.getElementById('amount1000').innerHTML = amount1000;
+   //Sender den oppdaterte scoren og amount100 til php scriptet
+   var xhr = new XMLHttpRequest();
+   xhr.open("POST", location.href, true);
+   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+   xhr.onreadystatechange = function() {
+     if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+       console.log("You got another upgrade", amount1000);
+     }
+   } 
+   xhr.send("score=" + score + "&amount1000=" + amount1000);
   }
  }
 
